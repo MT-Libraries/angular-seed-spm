a387c447c5e8da1cffe38847c92865078da4fc4a
Thonatos.Yang <thonatos.yang@gmail.com>
2014-11-04 20:29:54 +0800

    Update dir and di

--- apps/dist/angular-seed-spm/0.0.1/src/app-debug.js
+++ apps/dist/angular-seed-spm/0.0.1/src/app-debug.js
@@ -1,16 +1,14 @@
 define("angular-seed-spm/0.0.1/src/app-debug", [], function(require, exports, module) {
   'use strict';
-  var AngularSeedSpm = angular.module('ASS', ['ngRoute', 'ASS.home', 'ASS.intro', 'ASS.about', 'ASS.version']);
-  AngularSeedSpm.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.otherwise({
-      redirectTo: '/home'
-    });
-    $locationProvider.html5Mode(false);
+  var AngularSeedSpm = angular.module('ASS', ['ui.router', 'ASS.home', 'ASS.intro', 'ASS.about', 'ASS.version', 'ASS.service']);
+  AngularSeedSpm.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
   }]);
   var home = require("angular-seed-spm/0.0.1/src/home/home-debug");
   var about = require("angular-seed-spm/0.0.1/src/about/about-debug");
   var intro = require("angular-seed-spm/0.0.1/src/intro/intro-debug");
   var version = require("angular-seed-spm/0.0.1/src/components/ver/version-debug");
+  var service = require("angular-seed-spm/0.0.1/src/components/service/service-debug");
   angular.bootstrap(document, ['ASS']);
 });
 define("angular-seed-spm/0.0.1/src/home/home-debug", [], function(require, exports, module) {
@@ -19,18 +17,35 @@ define("angular-seed-spm/0.0.1/src/home/home-debug", [], function(require, expor
    * STATIC
    ********************************************************/
   var logger = require("angular-seed-spm/0.0.1/src/components/util/logger-debug");
-  var home = angular.module('ASS.home', ['ngRoute']);
+  var home = angular.module('ASS.home', ['ui.router']);
   /********************************************************
    * CONTROLLER
    ********************************************************/
-  home.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {
-      templateUrl: 'apps/src/home/main.html',
-      controller: 'MainCtrl'
-    });
+  home.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+      url: '/home',
+      views: {
+        '': {
+          templateUrl: 'apps/src/home/layout.html',
+          controller: 'HomeCtrl'
+        },
+        'head@home': {
+          templateUrl: "apps/src/home/head.html"
+        },
+        'main@home': {
+          templateUrl: 'apps/src/home/main.html',
+          controller: 'MainCtrl'
+        }
+      }
+    })
   }]);
-  home.controller('MainCtrl', [function() {
+  home.controller('HomeCtrl', [function() {
     logger.log('this is logger test');
+  }]);
+  home.controller('MainCtrl', ['$scope', 'shareService', function($scope, shareService) {
+    shareService.asy().then(function(response) {
+      console.log(response);
+    });
   }]);
   module.exports = home;
 });
@@ -68,9 +83,10 @@ define("angular-seed-spm/0.0.1/src/components/conf/conf-debug", [], function(req
 });
 define("angular-seed-spm/0.0.1/src/about/about-debug", [], function(require, exports, module) {
   'use strict';
-  var about = angular.module('ASS.about', ['ngRoute']);
-  about.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/about', {
+  var about = angular.module('ASS.about', ['ui.router']);
+  about.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('about', {
+      url: '/about',
       templateUrl: 'apps/src/about/main.html',
       controller: 'AboutCtrl'
     });
@@ -82,9 +98,10 @@ define("angular-seed-spm/0.0.1/src/intro/intro-debug", [], function(require, exp
   /**
    * Created by thonatos on 14-10-20.
    */
-  var intro = angular.module('ASS.intro', ['ngRoute']);
-  intro.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/intro', {
+  var intro = angular.module('ASS.intro', ['ui.router']);
+  intro.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('intro', {
+      url: '/intro',
       templateUrl: 'apps/src/intro/main.html',
       controller: 'IntroCtrl'
     });
@@ -117,4 +134,45 @@ define("angular-seed-spm/0.0.1/src/components/ver/version-directive-debug", [], 
     };
   }]);
   module.exports = versionDirective;
+});
+define("angular-seed-spm/0.0.1/src/components/service/service-debug", [], function(require, exports, module) {
+  /**
+   * Created by thonatos on 14-10-31.
+   */
+  var service = angular.module('ASS.service', ['ASS.service.shareService']);
+  var share = require("angular-seed-spm/0.0.1/src/components/service/shareService-debug");
+  module.exports = service;
+});
+define("angular-seed-spm/0.0.1/src/components/service/shareService-debug", [], function(require, exports, module) {
+  /**
+   * Created by thonatos on 14-10-31.
+   */
+  var shareService = angular.module('ASS.service.shareService', []).factory('shareService', ['$http', function($http) {
+    var _data = {};
+    return ({
+      add: _add,
+      get: _get,
+      asy: _async
+    });
+    // Public Method
+    function _add(parmas) {
+      _data = parmas;
+    }
+
+    function _get() {
+      return _data;
+    }
+
+    function _async() {
+      var promise = $http.get('data/test.json').then(function(response) {
+        // The then function here is an opportunity to modify the response
+        //console.log(response);
+        // The return value gets picked up by the then in the controller.
+        return response.data;
+      });
+      // Return the promise to the controller
+      return promise;
+    }
+  }]);
+  module.exports = shareService;
 });
